e10e7048223676196e95d71d1ee4619cbda86b69
GoldGromofon91 <nikitkaaahd@gmail.com>
2020-07-22 12:24:10 +0300

    new

---
+++ 13. Алгоритмы Python/less_1/task_5.py
@@ -0,0 +1,83 @@
+"""
+Задание 5.
+Задание на закрепление навыков работы со стеком
+
+Примечание: в этом задании вспомните ваши знания по работе с ООП
+и опирайтесь на пример урока
+
+Реализуйте структуру "стопка тарелок".
+
+Мы можем складывать тарелки в стопку и при превышении некоторого значения
+нужно начать складывать тарелки в новую стопку.
+
+Структура должна предусматривать наличие нескольких стеков.
+Создание нового стека происходит при достижении предыдущим стеком порогового значения.
+Реализуйте по аналогии с примером, рассмотренным на уроке, необходимые методы,
+для реализации это структуры, добавьте новые методы (не рассмотренные в примере с урока)
+для реализации этой задачи.
+
+После реализации структуры, проверьте ее работу на различных сценариях
+"""
+"""Пример создания стека через ООП"""
+
+
+class PlatesClass:
+	def __init__(self):
+		self.some_list = []
+
+	def clear(self):
+		return self.some_list.clear()
+
+	def is_empty(self):
+		return self.some_list == []
+
+	def add_el(self,elem):
+		self.some_list.append(elem)
+
+	def pop_el(self):
+		return self.some_list.pop()
+
+	def del_el(self,elem):
+		return self.some_list.remove(elem)
+
+	def get_last_val(self):
+		return self.some_list[len(self.some_list) - 1]
+
+	def get_val(self,el):
+		for i in range(len(self.some_list)):
+			if (el - 1) == i:
+				print('Объект №{} - {}'.format(el,self.some_list[i]))
+
+	def all(self):
+		for el in enumerate(self.some_list,1):
+			print (el)
+
+	def size(self):
+		return len(self.some_list)
+
+
+if __name__ == '__main__':
+	PlC_OBJ = PlatesClass()
+
+	# Проверяем что стек пустой
+	PlC_OBJ.clear()
+	print(PlC_OBJ.is_empty())
+	print(PlC_OBJ.size())
+
+	PlC_OBJ.add_el('Тарелка 1')
+	PlC_OBJ.add_el('Тарелка 2')
+	PlC_OBJ.add_el('Тарелка 3')
+	PlC_OBJ.add_el('Тарелка 4')
+	PlC_OBJ.add_el('Тарелка 5')
+	
+	PlC_OBJ.all()
+	print('Последний элемент - {}'.format(PlC_OBJ.get_last_val()))
+
+	PlC_OBJ.get_val(3)
+
+
+
+
+
+
+
