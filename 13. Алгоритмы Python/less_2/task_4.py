9fcef74ff9892a49fc32e546fefdf9d40dc3a924
GoldGromofon91 <nikitkaaahd@gmail.com>
2020-07-31 14:16:17 +0300

    dz_04

--- 13. Алгоритмы Python/less_2/task_4.py
+++ 13. Алгоритмы Python/less_2/task_4.py
@@ -9,3 +9,32 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+# Решение 1
+# start = 1 
+# usr_num = int(input('Введите количество элементов: '))
+
+# def rec_sum(num_start, count):
+# 	# print(num_start, count)
+# 	if count % 2 == 0 :
+# 		num_start = num_start *(-1)
+# 		# print(f'New number- {num_start}')
+# 	elif num_start < 0 and count % 2 !=0:
+# 		# new = num_start * (-1)
+# 		num_start = num_start *(-1)
+# 	if count == 1:
+# 		return num_start
+# 	else:
+# 		return rec_sum(-num_start/2, count - 1) + num_start
+
+# print (f'Количество элементов: {usr_num}, Сумма: {rec_sum(start,usr_num)}')
+
+# Решение 2 (без условий смены знака)
+start = 1 
+usr_num = int(input('Введите количество элементов: '))
+
+def rec_sum(num_start, count):
+	if count == 1:
+		return num_start
+	else:
+		return rec_sum(-num_start/2, count - 1) + num_start
+print (f'Количество элементов: {usr_num}, Сумма: {rec_sum(start,usr_num)}')
