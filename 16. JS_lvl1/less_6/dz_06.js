0d8060ce7e5e4074d4eedc8c18a9a256d960628e
GoldGromofon91 <nikitkaaahd@gmail.com>
2020-12-27 00:46:43 +0300

    next_step:renderBIN

--- 16. JS_lvl1/less_6/dz_06.js
+++ 16. JS_lvl1/less_6/dz_06.js
@@ -19,38 +19,118 @@
 
     
 */
-const pageUser = {
+const catalogUser = {
     settingCatalog: {
         workEnviroment: '.catalog_goods',
-        itemConteiner: '.item_goods',
-        buttonBuyGoods: '.button_items',
+        itemConteiner: 'item_goods',
+        buttonBuyGoods: 'button_items',
     },
 
+    dataObj: [
+        {
+            idGoods: 123,
+            imgSrc: 'img/goods.png',
+            nameGoods: 'Goods1',
+            priceGoods: 23500,
+            quantity: 2,
+        },
+        {
+            idGoods: 133,
+            imgSrc: 'img/goods.png',
+            nameGoods: 'Goods2',
+            priceGoods: 24500,
+            quantity: 3,
+        },
+        {
+            idGoods: 111,
+            imgSrc: 'img/goods.png',
+            nameGoods: 'Goods4',
+            priceGoods: 1500,
+            quantity: 1,
+        },
+        {
+            idGoods: 112,
+            imgSrc: 'img/goods.png',
+            nameGoods: 'Goods10',
+            priceGoods: 24500,
+            quantity: 3,
+        },
+        {
+            idGoods: 123,
+            imgSrc: 'img/goods.png',
+            nameGoods: 'Goods3',
+            priceGoods: 5500,
+            quantity: 1,
+        }],
+
+    initCatalog() {
+        this.dataObj.forEach(obj => {
+            this.renderElement(obj);
+        })
+        binUser.init();
+    },
+
+    renderElement(obj) {
+        const parentBlockGoods = document.querySelector(this.settingCatalog.workEnviroment);
+        return parentBlockGoods.insertAdjacentHTML('beforeend',
+            `<div class="${this.settingCatalog.itemConteiner}">
+                    <img src="${obj['imgSrc']}" alt="">
+                    <h4>${obj['nameGoods']}</h4>
+                    <p>${obj['priceGoods']}</p>
+                    <button class="${this.settingCatalog.buttonBuyGoods}">Buy</button>
+                  </div>`);
+    },
+}
+const binUser ={
     settingsBin: {
-        binBlockText: '.block_bin',
-        elementBinOl: '.ol_block_bin',
-        elementBinLi: '.li_block_bin',
-        binBlockCost: '.block_result_price',
+        parentBlock: '.bin',
+        binBlockText: 'block_bin',
+        imgBinSrc: 'img/shop.png',
+        elementBinOl: 'ol_block_bin',
+        elementBinLi: 'li_block_bin',
+        binBlockCost: 'block_result_price',
+        binElement: [],
     },
 
 
-    initWorkBlock() {
-        document.querySelectorAll(this.settingCatalog.buttonBuyGoods).forEach(
-            element => {
-                console.log(element);
-            addEventListener('click', event => {
-                this.checkClickUser(event)
-            })
-        });
-    },
-    checkClickUser(event) {
-        if(event.target.tagName !== 'BUTTON') return;
-        this.getParam(event);
-
+    init() {
+        this.renderBin();
     },
 
-    getParam(event) {
-        console.log(event.target)
+
+    renderBin() {
+        const parentBinBlock = document.querySelector(this.settingsBin.parentBlock);
+        return parentBinBlock.insertAdjacentHTML('beforeend',
+            `<div class="${this.settingsBin.binBlockText}">
+                      <ol class="${this.settingsBin.elementBinOl}">
+                          <li class="${this.settingsBin.elementBinLi}"></li>
+                      </ol>
+                  </div>
+                  <div class="${this.settingsBin.binBlockCost}">
+                      <img src="${this.settingsBin.imgBinSrc}" alt="">
+                      <p></p>
+                  </div>`);
     },
-};
-pageUser.initWorkBlock()
+}
+//
+
+//     initWorkBlock() {
+//         document.querySelectorAll(this.settingCatalog.buttonBuyGoods).forEach(
+//             element => {
+//                 // console.log(element);
+//             element.addEventListener('click', event => {
+//                 this.checkClickUser(event)
+//             })
+//         });
+//     },
+//     checkClickUser(event) {
+//         if(event.target.tagName !== 'BUTTON') return;
+//         this.getParam(event);
+//
+//     },
+//
+//     getParam(event) {
+//         console.log(event)
+//     },
+// };
+catalogUser.initCatalog()
