55dfa4932bceb689a6ec21fd81acb679f30454ef
GoldGromofon91 <nikitkaaahd@gmail.com>
2021-08-27 17:40:34 +0300

    change model

--- 21.Drf/server/creator/models.py
+++ 21.Drf/server/creator/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 # Create your models here.
 class Author(AbstractUser):
-    username = models.CharField(max_length=100,unique=True,null=False,blank=False)
-    email = models.CharField(max_length=48,unique=True,null=False,blank=False)
+    username = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    email = models.CharField(max_length=48, unique=True, null=False, blank=False)
     first_name = models.CharField(max_length=64, null=True, blank=True)
     last_name = models.CharField(max_length=64, null=True, blank=True)
     birthday_year = models.PositiveSmallIntegerField(null=True)
     is_active = models.BooleanField(default=False)
+
     def __str__(self):
         return self.username
 
     class Meta:
-        db_table='author'
-
+        db_table = 'author'
