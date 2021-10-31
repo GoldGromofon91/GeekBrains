6cecb0824494af902697eac2d8d187d2da768e8d
GoldGromofon91 <nikitkaaahd@gmail.com>
2021-10-31 20:22:14 +0300

    add README

--- 22.Chat/start.py
+++ 22.Chat/start.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
-import sys
-import time
+
 from os.path import dirname, abspath, join
 
 CHAT_DIR = abspath(join(dirname(os.path.abspath(__file__)), 'chat'))
@@ -10,12 +9,13 @@ CHAT_DIR = abspath(join(dirname(os.path.abspath(__file__)), 'chat'))
 def start():
     print(CHAT_DIR)
     print('Server start')
-    server_proc = subprocess.Popen(['python', f'{CHAT_DIR}/server.py'],shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.DEVNULL)
+    server_proc = subprocess.Popen(['python', f'{CHAT_DIR}/server.py'], shell=True, stdout=subprocess.PIPE,
+                                   stdin=subprocess.PIPE, stderr=subprocess.DEVNULL)
     server_proc.wait()
     our, err = server_proc.communicate()
     if not server_proc.returncode:
-        client_proc = subprocess.Popen(['python', f'{CHAT_DIR}/client.py'],shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
-
+        client_proc = subprocess.Popen(['python', f'{CHAT_DIR}/client.py'], shell=True, stdout=subprocess.PIPE,
+                                       stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 if __name__ == "__main__":
